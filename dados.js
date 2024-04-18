4ef2f92b782784f090da2bdc850dbebd1e9ed022
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-04-17 21:54:12 -0300

    ajustes

---
+++ dados.js
@@ -0,0 +1,156 @@
+var conteudos = {
+    "conteudos": [
+      {
+        "cod": "1",
+        "titulo": "Gouda com tinto",
+        "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus lectus, pretium sed fringilla vel, viverra sed dolor. Aenean nec elementum nulla, et dapibus ante. Nulla nec elementum sapien. In porta mattis nulla, nec lacinia metus sollicitudin vitae. In hac habitasse platea dictumst. Sed in magna tempor, vestibulum neque et, fermentum neque. Nullam nec vestibulum magna. Donec lectus justo, ornare porta dictum a, iaculis sit amet tortor. Nullam suscipit dapibus ante sed imperdiet. Mauris massa nulla, congue ac diam id, laoreet tempor velit. Nam vestibulum rhoncus magna id fermentum. Morbi convallis magna ut mi molestie, et commodo ligula mollis. Proin et lacus augue. Donec pellentesque pellentesque massa. Nulla imperdiet lacus non augue elementum ornare. Curabitur porttitor tempus arcu, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus lectus, pretium sed fringilla vel, viverra sed dolor. Aenean nec elementum nulla, et dapibus ante. Nulla nec elementum sapien. In porta mattis nulla, nec lacinia metus sollicitudin vitae. In hac habitasse platea dictumst. Sed in magna tempor, vestibulum neque et, fermentum neque. Nullam nec vestibulum magna. Donec lectus justo, ornare porta dictum a, iaculis sit amet tortor. Nullam suscipit dapibus ante sed imperdiet. Mauris massa nulla, congue ac diam id, laoreet tempor velit. Nam vestibulum rhoncus magna id fermentum. Morbi convallis magna ut mi molestie, et commodo ligula mollis. Proin et lacus augue. Donec pellentesque pellentesque massa. Nulla imperdiet lacus non augue elementum ornare. Curabitur porttitor tempus arcu, ",
+        "chamada": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus lectus, pretium sed fringilla vel, viverra sed dolor. Aenean nec elementum nulla, et dapibus ante. Nulla nec elementum sapien",
+        "imagem": "img/macarrao.jpg"
+      }
+    ]
+  }
+  
+  var loja = {
+    "pratos": [
+      {
+        "cod": "1",
+        "nome": "Espaguete da Felicidade",
+        "descricao": "Ingredientes: Espaguete com almôndegas em forma de sorrisos e molho de tomate. Valor nutricional: Proteínas (carne), carboidratos (macarrão) e gordura (molho). Alergênicos: Glúten (macarrão), carne.",
+        "preco": "29,90",
+        "imagem": "img/macarrao.jpg",
+        "categoria": "principal",
+        "avaliacoes": [
+          {
+            "nome": "Thiago Ventura",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "2",
+        "nome": "Risoto Risível",
+        "descricao": "Ingredientes: Risoto de cogumelos com parmesão em formato de palhaço. Valor nutricional: Proteínas (queijo), carboidratos (arroz) e gordura (queijo). Alergênicos: Laticínios (queijo).",
+        "preco": "29,90",
+        "imagem": "img/risoto.jpg",
+        "categoria": "principal",
+        "avaliacoes": [
+          {
+            "nome": "Fábio Porchat",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "3",
+        "nome": "Pizzas Engraçadas",
+        "descricao": "Ingredientes: Pizzas em formatos divertidos com diversas coberturas e queijo derretido. Valor nutricional: Proteínas (queijo e carnes), carboidratos (massa) e gordura (queijo e carnes).Alergênicos: Glúten (massa), laticínios (queijo",
+        "preco": "29,90",
+        "imagem": "img/pizza.jpg",
+        "categoria": "principal",
+        "avaliacoes": [
+          {
+            "nome": "Bruna louise",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "4",
+        "nome": "Salada de Risadas",
+        "descricao": "Ingredientes: Salada de frutas frescas com molho de iogurte e granola em formato de sorrisos. Valor nutricional: Rica em fibras e vitaminas (frutas e granola), moderada em gordura (iogurte).Alergênicos: Laticínios (iogurte).",
+        "preco": "12,90",
+        "imagem": "img/salada.jpg",
+        "categoria": "entradas",
+        "avaliacoes": [
+          {
+            "nome": "Victor Sarro",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          },
+          {
+            "nome": "Victor Sarro",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 4.5
+          }
+        ]
+      },
+      {
+        "cod": "5",
+        "nome": "Pão do bobo",
+        "descricao": "Ingredientes: Pão recheado com queijo derretido e bacon crocante. Valor nutricional: Proteínas (queijo e bacon), carboidratos (pão), gordura (bacon). Alergênicos: Glúten (pão), laticínios (queijo), carne (bacon).",
+        "preco": "14,50",
+        "imagem": "img/pao.jpg",
+        "categoria": "entradas",
+        "avaliacoes": [
+          {
+            "nome": "Fábio Porchat",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "6",
+        "nome": "Tiras de riso",
+        "descricao": "Ingredientes: Tiras de queijo empanadas servidas com molho de pimenta Valor nutricional: Rica em proteínas (queijo), gordura (empanado) e carboidratos (molho de pimenta). Alergênicos: Laticínios (queijo), glúten (empanado).",
+        "preco": "14,50",
+        "imagem": "img/queijo.jpg",
+        "categoria": "entradas",
+        "avaliacoes": [
+          {
+            "nome": "Bruna louise",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "7",
+        "nome": "Limona",
+        "descricao": "Ingredientes: Limonada com gás em garrafas com rótulos divertidos. Valor nutricional: Baixas calorias. Fonte de vitamina C. Alergênicos: Nenhum.",
+        "preco": "14,00",
+        "imagem": "img/limonada.jpg",
+        "categoria": "bebidas",
+        "avaliacoes": [
+          {
+            "nome": "Victor Sarro",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "8",
+        "nome": "Suco do Sorriso",
+        "descricao": "Ingredientes: Suco de laranja e manga servido em copos com canudos engraçados. Valor nutricional: Vitaminas das frutas, baixas calorias. Alergênicos: Nenhum.",
+        "preco": "14,90",
+        "imagem": "img/suco.jpg",
+        "categoria": "bebidas",
+        "avaliacoes": [
+          {
+            "nome": "Bruna louise",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+      {
+        "cod": "9",
+        "nome": "Coquetel do Bobo",
+        "descricao": "Ingredientes: Coquetel de frutas com guarda-chuvas coloridos. Valor nutricional: Baixas calorias. Alergênicos: Pode variar de acordo com os ingredientes.",
+        "preco": "19,90",
+        "imagem": "img/drink.jpg",
+        "categoria": "bebidas",
+        "avaliacoes": [
+          {
+            "nome": "Thiago Ventura",
+            "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
+            "nota": 3.5
+          }
+        ]
+      },
+    ]
+  }
--- script.js
+++ script.js
@@ -1,46 +1,3 @@
-function smoothScroll(target, duration) {
-  var target = document.querySelector(target);
-  var targetPosition = target.getBoundingClientRect().top;
-  var startPosition = window.pageYOffset;
-  var distance = targetPosition - startPosition;
-  var startTime = null;
-
-  function animation(currentTime) {
-    if (startTime === null) startTime = currentTime;
-    var timeElapsed = currentTime - startTime;
-    var run = easing(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, run);
-    if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
-
-  function easing(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
-
-  requestAnimationFrame(animation);
-}
-
-var about = document.querySelector('#sobre');
-var contact = document.querySelector('#contato');
-
-var aboutLink = document.querySelector('nav a[href="#sobre"]');
-var contactLink = document.querySelector('nav a[href="#contato"]');
-
-if (aboutLink != null) {
-  aboutLink.addEventListener('click', function () {
-    smoothScroll('#sobre', 1000);
-  });
-}
-
-if (contactLink != null) {
-  contactLink.addEventListener('click', function () {
-    smoothScroll('#contato', 1000);
-  });
-}
-
 const $dropdown = $(".dropdown");
 const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
@@ -82,163 +39,6 @@ if (document.getElementById("titulo_loja") != null) {
 
 var cod = parseInt(searchParams.get('cod'))
 console.log(cod);
-
-var conteudos = {
-  "conteudos": [
-    {
-      "cod": "1",
-      "titulo": "Gouda com tinto",
-      "texto": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus lectus, pretium sed fringilla vel, viverra sed dolor. Aenean nec elementum nulla, et dapibus ante. Nulla nec elementum sapien. In porta mattis nulla, nec lacinia metus sollicitudin vitae. In hac habitasse platea dictumst. Sed in magna tempor, vestibulum neque et, fermentum neque. Nullam nec vestibulum magna. Donec lectus justo, ornare porta dictum a, iaculis sit amet tortor. Nullam suscipit dapibus ante sed imperdiet. Mauris massa nulla, congue ac diam id, laoreet tempor velit. Nam vestibulum rhoncus magna id fermentum. Morbi convallis magna ut mi molestie, et commodo ligula mollis. Proin et lacus augue. Donec pellentesque pellentesque massa. Nulla imperdiet lacus non augue elementum ornare. Curabitur porttitor tempus arcu, ",
-      "chamada": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus lectus, pretium sed fringilla vel, viverra sed dolor. Aenean nec elementum nulla, et dapibus ante. Nulla nec elementum sapien",
-      "imagem": "img/macarrao.jpg"
-    }
-  ]
-}
-
-var loja = {
-  "pratos": [
-    {
-      "cod": "1",
-      "nome": "Espaguete da Felicidade",
-      "descricao": "Ingredientes: Espaguete com almôndegas em forma de sorrisos e molho de tomate. Valor nutricional: Proteínas (carne), carboidratos (macarrão) e gordura (molho). Alergênicos: Glúten (macarrão), carne.",
-      "preco": "29,90",
-      "imagem": "img/macarrao.jpg",
-      "categoria": "principal",
-      "avaliacoes": [
-        {
-          "nome": "Thiago Ventura",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "2",
-      "nome": "Risoto Risível",
-      "descricao": "Ingredientes: Risoto de cogumelos com parmesão em formato de palhaço. Valor nutricional: Proteínas (queijo), carboidratos (arroz) e gordura (queijo). Alergênicos: Laticínios (queijo).",
-      "preco": "29,90",
-      "imagem": "img/risoto.jpg",
-      "categoria": "principal",
-      "avaliacoes": [
-        {
-          "nome": "Fábio Porchat",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "3",
-      "nome": "Pizzas Engraçadas",
-      "descricao": "Ingredientes: Pizzas em formatos divertidos com diversas coberturas e queijo derretido. Valor nutricional: Proteínas (queijo e carnes), carboidratos (massa) e gordura (queijo e carnes).Alergênicos: Glúten (massa), laticínios (queijo",
-      "preco": "29,90",
-      "imagem": "img/pizza.jpg",
-      "categoria": "principal",
-      "avaliacoes": [
-        {
-          "nome": "Bruna louise",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "4",
-      "nome": "Salada de Risadas",
-      "descricao": "Ingredientes: Salada de frutas frescas com molho de iogurte e granola em formato de sorrisos. Valor nutricional: Rica em fibras e vitaminas (frutas e granola), moderada em gordura (iogurte).Alergênicos: Laticínios (iogurte).",
-      "preco": "12,90",
-      "imagem": "img/salada.jpg",
-      "categoria": "entradas",
-      "avaliacoes": [
-        {
-          "nome": "Victor Sarro",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        },
-        {
-          "nome": "Victor Sarro",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 4.5
-        }
-      ]
-    },
-    {
-      "cod": "5",
-      "nome": "Pão do bobo",
-      "descricao": "Ingredientes: Pão recheado com queijo derretido e bacon crocante. Valor nutricional: Proteínas (queijo e bacon), carboidratos (pão), gordura (bacon). Alergênicos: Glúten (pão), laticínios (queijo), carne (bacon).",
-      "preco": "14,50",
-      "imagem": "img/pao.jpg",
-      "categoria": "entradas",
-      "avaliacoes": [
-        {
-          "nome": "Fábio Porchat",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "6",
-      "nome": "Tiras de riso",
-      "descricao": "Ingredientes: Tiras de queijo empanadas servidas com molho de pimenta Valor nutricional: Rica em proteínas (queijo), gordura (empanado) e carboidratos (molho de pimenta). Alergênicos: Laticínios (queijo), glúten (empanado).",
-      "preco": "14,50",
-      "imagem": "img/queijo.jpg",
-      "categoria": "entradas",
-      "avaliacoes": [
-        {
-          "nome": "Bruna louise",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "7",
-      "nome": "Limona",
-      "descricao": "Ingredientes: Limonada com gás em garrafas com rótulos divertidos. Valor nutricional: Baixas calorias. Fonte de vitamina C. Alergênicos: Nenhum.",
-      "preco": "14,00",
-      "imagem": "img/limonada.jpg",
-      "categoria": "bebidas",
-      "avaliacoes": [
-        {
-          "nome": "Victor Sarro",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "8",
-      "nome": "Suco do Sorriso",
-      "descricao": "Ingredientes: Suco de laranja e manga servido em copos com canudos engraçados. Valor nutricional: Vitaminas das frutas, baixas calorias. Alergênicos: Nenhum.",
-      "preco": "14,90",
-      "imagem": "img/suco.jpg",
-      "categoria": "bebidas",
-      "avaliacoes": [
-        {
-          "nome": "Bruna louise",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-    {
-      "cod": "9",
-      "nome": "Coquetel do Bobo",
-      "descricao": "Ingredientes: Coquetel de frutas com guarda-chuvas coloridos. Valor nutricional: Baixas calorias. Alergênicos: Pode variar de acordo com os ingredientes.",
-      "preco": "19,90",
-      "imagem": "img/drink.jpg",
-      "categoria": "bebidas",
-      "avaliacoes": [
-        {
-          "nome": "Thiago Ventura",
-          "avaliação": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ullamcorper euismod euismod. Nunc ac scelerisque tellus. Aenean sed convallis orci, eu pharetra ante. Vivamus sapien nisl, euismod ac varius eu, dignissim id dui. Aenean ut eros sed mi malesuada varius. Vestibulum suscipit bibendum quam non faucibus. Praesent auctor, nibh vel luctus cursus, ipsum urna pharetra mauris, eget pretium erat libero quis eros. In porta magna tortor, consectetur ultrices tortor aliquet vel. Fusce malesuada magna erat, ut blandit tortor mattis et.",
-          "nota": 3.5
-        }
-      ]
-    },
-  ]
-}
 
 var prato = loja["pratos"][cod - 1]
 console.log(prato)
@@ -309,6 +109,7 @@ if (document.getElementById("titulo_conteudo") != null) {
   document.getElementById("titulo_conteudo").innerHTML = conteudoExibir.titulo
   document.getElementById("texto_conteudo").innerHTML = conteudoExibir.texto  
   document.getElementById("imagem_conteudo").src = conteudoExibir.imagem
+  // document.getElementById("conteudoVer").innerHTML = "<img src='img/macarrao.jpg' id='imagem_conteudo'>" + conteudoExibir.texto  
 }
 
 if (document.getElementById("conteudos") != null) {
@@ -316,9 +117,7 @@ if (document.getElementById("conteudos") != null) {
 }
 
 
-if (document.getElementById("pratos") != null) {
-  document.getElementById("pratos").innerHTML = container
-}
+
 
 jQuery(document).ready(function () {
 
@@ -427,6 +226,7 @@ function carregarCarrinho() {
       `;
   });
 
+
   // Adiciona a tabela à div do carrinho
   carrinhoDiv.appendChild(tabela);
   document.getElementById("subtotal").innerText = "R$ " + subtotal.toFixed(2).toString().replace(".", ",");
@@ -526,3 +326,4 @@ function adicionarProduto() {
   });
 
 }
+
